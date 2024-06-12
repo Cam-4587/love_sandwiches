10a5d4b602058c53a6fa4b3335a34a053437ec56
Cam-4587 Conroy <cameronconroy@live.com>
2024-06-12 16:32:55 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
 def get_sales_data():
     """
     Get slaes figures input from the user.
@@ -77,16 +78,25 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data... \n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
+
 
 def main():
     """
-    Run all pprogram functions
+    Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 print("Welcome to love Sandwiches Data Automation")
 main()
