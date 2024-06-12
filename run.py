adb445779184a208231e718c0fa83fdd845b1d6b
Cam-4587 Conroy <cameronconroy@live.com>
2024-06-12 13:02:27 +0000

    Add user reuest loop to repeat reuest for data until provided is valid

--- run.py
+++ run.py
@@ -13,20 +13,25 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
-
 def get_sales_data():
     """
     Get slaes figures input from the user
-
     """
-    print("Please enter sales data from the the last market.")
-    print("Data shold be siz numbers, separated by commas")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the the last market.")
+        print("Data shold be siz numbers, separated by commas")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: ")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+        
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
+    
+    return sales_data
+
 
 def validate_data(values):
     """
@@ -42,8 +47,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again\n")
-                
+        return False
+    return True          
             
 
 
-get_sales_data()
+data = get_sales_data()
